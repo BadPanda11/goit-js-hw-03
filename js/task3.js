c73b908b45862be17bb06a2ce9e00dbb655309ae
Rodion Mokan <mokan11r@gmail.com>
2020-05-07 01:41:48 +0300

    1

---
+++ js/task3.js
@@ -0,0 +1,23 @@
+const findBestEmployee = function (employees) {
+  console.log(employees);
+  let searchBestEmployee = 0;
+  let bestEmployee;
+  for (const employee in employees) {
+    if (employees[employee] > searchBestEmployee) {
+      bestEmployee = employee;
+    }
+  }
+  return bestEmployee;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99,
+  }),
+); // lorence
